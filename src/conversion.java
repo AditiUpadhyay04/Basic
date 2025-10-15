e622f150f5d6df748add3115dd44f908ba462d85
asus <aditiupadhyay102@gmail.com>
2025-10-15 19:33:29 +0530

    Second

---
+++ src/conversion.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class conversion {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        sc.nextLine();
+        String name = sc.nextLine();
+       // sc.nextLine();
+       // int a = sc.nextInt();
+        System.out.println(a);
+        System.out.println(name);
+       // System.out.println(a);
+    }
+}
