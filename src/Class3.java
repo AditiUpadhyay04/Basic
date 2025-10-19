1bdf1c4c91e2281892d2c42686ffb6e0d0db6748
AditiUpadhyay04 <aditiupadhyay102@gmail.com>
2025-10-19 23:26:03 +0530

    Three

---
+++ src/Class3.java
@@ -0,0 +1,20 @@
+public class Class3 {
+    public static void main(String[] args){
+        //System.out.println("Aditi\n");
+        int a = 12; // Declaration and Initialization
+        int b = 31;
+        int Sum = a + b;
+        System.out.println("Sum is :" +Sum);
+
+        a = 75; // Upadation
+        b = 12;
+        int subtract = a - b;
+        System.out.println("Subtract is :" +subtract);
+
+        int Multiply = a * b;
+        System.out.println("Multiply is :" +Multiply);
+
+        double Divide = a / b ;
+        System.out.println("Divide is :" +Divide);
+    }
+}
