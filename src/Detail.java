69e29dd489db7e62398a424f4b7d6b43e4aa0694
AditiUpadhyay04 <aditiupadhyay102@gmail.com>
2025-10-19 23:26:53 +0530

    Three

---
+++ src/Detail.java
@@ -0,0 +1,12 @@
+public class Detail {
+    public static void main(String[] args) {
+        int rollno = 8;
+        int age = 20;
+        String name = "Aditi";
+        char inital = name.charAt(0);
+
+        System.out.println(rollno);
+        System.out.println(age);
+        System.out.println(inital);
+    }
+};
