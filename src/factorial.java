400343cf99e688c41abfe543a8232d93d9e1dcf4
AditiUpadhyay04 <aditiupadhyay102@gmail.com>
2025-10-19 23:27:46 +0530

    Three

---
+++ src/factorial.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class factorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int factorial= 1;
+        int i = 1;
+        while(i <= num){
+            factorial = i * factorial;
+            i++;
+
+        }
+        System.out.println(+factorial);
+    }
+}
