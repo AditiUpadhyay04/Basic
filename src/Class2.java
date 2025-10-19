bd05fdf7c83ab9111f5716c8c43b61e61e764916
AditiUpadhyay04 <aditiupadhyay102@gmail.com>
2025-10-19 23:25:42 +0530

    Three

---
+++ src/Class2.java
@@ -0,0 +1,57 @@
+import java.sql.SQLOutput;
+
+public class Class2 {
+    public static void main(String[] args){
+        long a = 32656465L;
+        char b = (char)a;
+        System.out.println(b);
+
+        float c = 836.373F;
+        double d = c;
+        System.out.println(d);
+
+        short e = 564;
+        long f = e;
+        System.out.println(f);
+
+        char x = 'q';
+        int y = (int)x;
+        System.out.println(y);
+
+        char z = 'w';
+        short m = (short)z;
+        System.out.println(m);
+
+        short p = 764;
+        int q = (int)p;
+        System.out.println(q);
+
+        int r = 537554556;
+        long s = (long)r;
+        System.out.println(s);
+
+        long t = 5637655378263L;
+        float u = (float)t;
+        System.out.println(u);
+
+        float g = 4364723F;
+        long h = (long)g;
+        System.out.println(h);
+
+        long i = 66535476977667442L;
+        int j = (int)i;
+        System.out.println(j);
+
+        int k = 45342;
+        short l = (short)k;
+        System.out.println(l);
+
+        double o = 5764.3767d;
+        float v = (float)o;
+        System.out.println(v);
+
+        short w = 22756;
+        char n = (char)w;
+        System.out.println(n);
+    }
+}
