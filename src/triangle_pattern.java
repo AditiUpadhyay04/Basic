b88d5ff07985e2251b501d634523af78672dd68c
AditiUpadhyay04 <aditiupadhyay102@gmail.com>
2025-10-22 23:28:58 +0530

    Three

---
+++ src/triangle_pattern.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class triangle_pattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 0; i < 2 * n - 1; i++){
+            int stars = i + 1;
+            if(i >= n) stars = 2 * n - i - 1;
+                 for(int j = 0; j < stars; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
