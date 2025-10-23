8a309115834ee42015ef332f3393a0d7aa8b0be1
AditiUpadhyay04 <aditiupadhyay102@gmail.com>
2025-10-23 21:49:38 +0530

    Three

---
+++ src/dowhile.java
@@ -0,0 +1,10 @@
+public class dowhile {
+    public static void main(String[] args) {
+        int i = 1;
+        int num = 27;
+        do {
+            System.out.println(num + "x" + i + "=" + (num * i));
+           i++;
+        } while (i <= 10);
+    }
+}
