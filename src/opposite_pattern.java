a96c0c08fd629365b5175505a6a1194592018b01
AditiUpadhyay04 <aditiupadhyay102@gmail.com>
2025-10-27 00:33:24 +0530

    Three

---
+++ src/opposite_pattern.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+public class opposite_pattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for ( int i = 1; i <= n; i++){
+
+            for(int j = 1; j<= i; j++){
+                System.out.print(" ");
+            }
+            for(int j = 1; j <= 2 * n - 2 * i + 1; j++){
+                System.out.print("*");
+            }
+            for(int j = 1; j <= n- i - 1; j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+
+        for ( int i = 1; i <= n; i++){
+
+            for(int j = 1; j<= i; j++){
+                System.out.print(" ");
+            }
+            for(int j = 1; j <= 2 * n - 2 * i + 1; j++){
+                System.out.print("*");
+            }
+            for(int j = 1; j <= n- i - 1; j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+        for (int i = 1; i <= n; i++) {
+
+            // print initial spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            // print start
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+
+            // print remaining spaces
+            for (int j = 1; j <= n - i; j++) {
+                //   System.out.print();
+            }
+            System.out.println();
+
+        }
+}
+}
