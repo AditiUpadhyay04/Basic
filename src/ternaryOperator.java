d6ce1607e0eb7f73e55cd1912416373a7d0117c7
AditiUpadhyay04 <aditiupadhyay102@gmail.com>
2025-11-05 23:27:30 +0530

    Three

---
+++ src/ternaryOperator.java
@@ -0,0 +1,12 @@
+public class ternaryOperator {
+    public static void main(String[] args) {
+        int a = 23;
+        int b = 76;
+        String W = (a == b) ? "they are equal" : "they are not equal";
+        System.out.println(W);
+
+        System.out.println((a != b) ? "True" : "False" );
+
+        System.out.println((a == b) ? 0 : 1);
+    }
+}
