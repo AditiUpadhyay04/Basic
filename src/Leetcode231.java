c3dbfb898c94036e2e7ab8ec5520118c97810951
AditiUpadhyay04 <aditiupadhyay102@gmail.com>
2026-01-15 21:44:51 +0530

    Three

---
+++ src/Leetcode231.java
@@ -0,0 +1,20 @@
+public class Leetcode231 {
+    public boolean isPowerOfTwo(int n) {
+            if(n <= 0)
+            {
+
+                return false;
+
+            }
+            while(n % 2 == 0)
+            {
+                n = n / 2 ;
+            }
+            return n == 1;
+        }
+
+    public static void main(String[] args) {
+
+    }
+}
+
